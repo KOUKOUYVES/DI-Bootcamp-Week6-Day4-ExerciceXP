d012fb28221cf48c86cfb3fdcbd1536cf433c1ee
KOUKOUYVES <wogelisabeth@gmail.com>
2023-02-03 17:21:18 +0000

    first

--- exercice2/index.php
+++ exercice2/index.php
@@ -11,28 +11,12 @@
 <br><br>
 <h1 style="text-align: center;">ExerciceXP 2 Calculate Area Of Rectangle</h1>
 
-<div class="container">
-        <form method="post">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Entrer un nombre</label>
-                <input type="text" name="str[]" value="<?php echo $fruits[$key];?>" class="form-control">
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary">Delete</button>
-        </form><br>
+<div class="container"><br>
 
         <?php
                 $fruits = array("Janvier <br>", "Fevrier <br>", "Mars <br>", "Avril <br>", "Mai <br>", "Juin <br>", "Juillet <br>", "Août <br>", "Septembre <br>", "Octobre <br>", "Novembre <br>", "Décembre <br>");
-                    foreach($fruits as $key  => $fruit):
-                        echo $key.'-'.$fruit."<br>";
-                    endforeach;
-                    if (isset($_POST['submit'])) {
-                        $input = $_POST["str"];
-                        unset($fruits[$key]);
-                        }
-
-        // $langages = array("p"=>"PHP", "j"=>"Java", "a"=>"Ada", "h"=>"HTML");
-        // unset($langages["j"]);
-        // print_r($langages); 
+                        unset($fruits[2]);
+                        print_r($fruits); 
         ?>
 </div>
 
